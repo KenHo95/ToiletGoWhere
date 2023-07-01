6c67ea18c3a4b541f210b3e6bf1c0db0bc5ca610
coeyyt <coeytky@gmail.com>
2023-07-01 12:20:22 +0800

    added in header

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -8,34 +8,34 @@ import "../App.css";
 import React, { useState } from "react";
 
 import toiletIcon from "../toileticon.png";
-import ToiletList from "./ToiletList";
+// import ToiletList from "./ToiletList";
 
 const Map = (props) => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   });
-  const [mapRef, setMapRef] = useState();
-  const [isOpen, setIsOpen] = useState(false);
-  const [infoWindowData, setInfoWindowData] = useState();
+  // const [mapRef, setMapRef] = useState();
+  // const [isOpen, setIsOpen] = useState(false);
+  // const [infoWindowData, setInfoWindowData] = useState();
 
-  const markers = [
-    { address: "Bishan Bus Interchange", lat: 1.350139, lng: 103.849757 },
-    { address: "Harbourfront Bus Terminal", lat: 1.266871, lng: 103.819193 },
-    { address: "Yio Chu Kang Bus Interchange", lat: 1.373912, lng: 103.849592 },
-  ];
+  // const markers = [
+  //   { address: "Bishan Bus Interchange", lat: 1.350139, lng: 103.849757 },
+  //   { address: "Harbourfront Bus Terminal", lat: 1.266871, lng: 103.819193 },
+  //   { address: "Yio Chu Kang Bus Interchange", lat: 1.373912, lng: 103.849592 },
+  // ];
 
-  const onMapLoad = (map) => {
-    setMapRef(map);
-    const bounds = new window.google.maps.LatLngBounds();
-    markers?.forEach(({ lat, lng }) => bounds.extend({ lat, lng }));
-    map.fitBounds(bounds);
-  };
+  // const onMapLoad = (map) => {
+  //   setMapRef(map);
+  //   const bounds = new window.google.maps.LatLngBounds();
+  //   markers?.forEach(({ lat, lng }) => bounds.extend({ lat, lng }));
+  //   map.fitBounds(bounds);
+  // };
 
-  const handleMarkerClick = (id, lat, lng, address) => {
-    mapRef?.panTo({ lat, lng });
-    setInfoWindowData({ id, address });
-    setIsOpen(true);
-  };
+  // const handleMarkerClick = (id, lat, lng, address) => {
+  //   mapRef?.panTo({ lat, lng });
+  //   setInfoWindowData({ id, address });
+  //   setIsOpen(true);
+  // };
 
   return (
     <div>
@@ -45,8 +45,8 @@ const Map = (props) => {
         ) : (
           <GoogleMap
             mapContainerClassName="map-container"
-            onLoad={onMapLoad}
-            onClick={() => setIsOpen(false)}
+            onLoad={props.onMapLoad}
+            onClick={() => props.setIsOpen(false)}
           >
             {props.toiletsData.map(
               ({ Address, Area, Name, Type, lat, lng }, Ind) => (
@@ -58,16 +58,18 @@ const Map = (props) => {
                     scaledSize: new window.google.maps.Size(50, 50),
                   }}
                   onClick={() => {
-                    handleMarkerClick(Ind, lat, lng, Address);
+                    props.handleMarkerClick(Ind, lat, lng, Address);
                   }}
                 >
-                  {isOpen && infoWindowData?.id === Ind && (
+                  {props.isOpen && props.infoWindowData?.id === Ind && (
                     <InfoWindow
                       onCloseClick={() => {
-                        setIsOpen(false);
+                        props.setIsOpen(false);
                       }}
                     >
-                      <h3>{infoWindowData.address}</h3>
+                      <div style={{ color: "red" }}>
+                        <h3>{props.infoWindowData.address}</h3>
+                      </div>
                     </InfoWindow>
                   )}
                 </MarkerF>
@@ -76,14 +78,14 @@ const Map = (props) => {
           </GoogleMap>
         )}
       </div>
-      <ToiletList
+      {/* <ToiletList
         toiletsData={props.toiletsData}
         usersLikesData={props.usersLikesData}
         setselectedToilet={props.setselectedToilet}
         setselectedToiletAddress={props.setselectedToiletAddress}
         userEmail={props.userEmail}
         handleMarkerClick={handleMarkerClick}
-      />
+      /> */}
     </div>
   );
 };
