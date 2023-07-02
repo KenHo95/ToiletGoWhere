21cfc2b98b5eebb39a8b0da289d5f70248a99036
KenHo95 <mingquan.ho.2017@accountancy.smu.edu.sg>
2023-07-02 18:31:49 +0800

    Cleaned duplicate states

--- src/Components/LikedToiletList.js
+++ src/Components/LikedToiletList.js
@@ -1,39 +1,14 @@
-import { React, useState, useEffect } from "react";
+import { React } from "react";
 import { useParams } from "react-router-dom";
-import { realTimeDatabase } from "../firebase";
-import { ref as realTimeDatabaseRef, onChildAdded } from "firebase/database";
 import ToiletList from "./ToiletList";
 
-const DB_APPDATA_KEY = "AppData";
-
 function LikedToiletList(props) {
-  const [likedToiletData, setLikedToiletData] = useState({});
   let { id } = useParams();
-
-  const LikedToiletRef = realTimeDatabaseRef(
-    realTimeDatabase,
-    DB_APPDATA_KEY + `/LikedToilets/${props.userEmail.split(".")[0]}/`
-  );
-
-  useEffect(() => {
-    props.userEmail &&
-      onChildAdded(LikedToiletRef, (data) => {
-        console.log("LikedToilet added");
-
-        setLikedToiletData((prev) => ({
-          ...prev,
-          [data.key]: data.val(),
-        }));
-      });
-
-    return () => {};
-  }, [props.userEmail]);
 
   return (
     <div>
       <ToiletList
         toiletsToDisplay={props.toiletsToDisplay}
-        likedToiletData={likedToiletData}
         usersLikesData={props.usersLikesData}
         urlId={id}
         userEmail={props.userEmail}
